b2742b3d26ee2feb42601f16b63f5aa9d4eb19e0
SiRa <rana.siddharth994@gmail.com>
2023-04-27 13:40:52 +0530

    get api for getting server info, state management for rendering data

--- src/modules/servers/create-servers/index.js
+++ src/modules/servers/create-servers/index.js
@@ -3,55 +3,55 @@ import { Form, Input, Button, Modal, message } from 'antd';
 import { CheckCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
-const ModalForm = () => {
+const ModalForm = (props) => {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [form] = Form.useForm();
     const showModal = () => {
         setIsModalVisible(true);
     };
     const onFinish = (values) => {
+        // create unique id using ms
         const id =  Date.now() % 10000;
         values['id'] = id;
-        
-        console.log(values);
+
         // submit form data to server
-        const saveData = axios.post(`${process.env.REACT_APP_BASE_API_ENDPOINT}/servers`,values);
-        saveData
-        .then((result) => {
-            if (result?.status === 200) {
-                console.log(result)
-                message.success({
-                    content: 'Form submitted successfully',
-                    icon: <CheckCircleOutlined />,
-                    duration: 3 // Duration in seconds to show the message
-                    });
-                setIsModalVisible(false);
-            }
-            else if (result?.status === 400) {
+        axios.post(`${process.env.REACT_APP_BASE_API_ENDPOINT}/servers`,values)
+            .then((result) => {
+                if (result?.status === 200) {
+                    // update state
+                    props.handler(result);  
+                    // success-popup
+                    message.success({
+                        content: 'Form submitted successfully',
+                        icon: <CheckCircleOutlined />,
+                        duration: 3 // Duration in seconds to show the message
+                        });
+                    setIsModalVisible(false);
+                }
+                else if (result?.status === 400) {
+                    message.error({
+                        content: 'Failed to submit form',
+                        icon: <ExclamationCircleOutlined />,
+                        duration: 3
+                    })
+                    setIsModalVisible(false);
+                }
+                else {
+                    message.error({
+                        content: 'Failed to submit form',
+                        icon: <ExclamationCircleOutlined />,
+                        duration: 3
+                    })
+                    setIsModalVisible(false);
+                }
+            }).catch(err => {
                 message.error({
                     content: 'Failed to submit form',
                     icon: <ExclamationCircleOutlined />,
                     duration: 3
                 })
-                setIsModalVisible(false);
-            }
-            else {
-                message.error({
-                    content: 'Failed to submit form',
-                    icon: <ExclamationCircleOutlined />,
-                    duration: 3
-                })
-                setIsModalVisible(false);
-            }
-        }).catch(err => {
-            message.error({
-                content: 'Failed to submit form',
-                icon: <ExclamationCircleOutlined />,
-                duration: 3
-            })
             setIsModalVisible(false);
         })
-
         form.resetFields(); // Clear the form data
     };
     return (
